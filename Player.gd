fbec1b7d0d1c2c1118a2cdd897a3d94d83093cec
Kamran Haq <KamranHaq@protonmail.com>
2022-10-17 19:07:46 -0700

    Removed print statement

--- Player.gd
+++ Player.gd
@@ -4,5 +4,4 @@ func _on_Hurtbox_area_entered(area):
 	var projectile = area.get_parent()
 	if projectile.is_in_group("enemy"):
 		take_damage(projectile.damage) 
-	print(self.current_hull_integrity)
-	print(self.current_shield_integrity)
+

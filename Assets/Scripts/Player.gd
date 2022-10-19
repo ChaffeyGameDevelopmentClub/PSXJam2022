d89026cdb2b89d22e0d4990617f2ba0329c901d2
Bdog12 <brandonjo1034@gmail.com>
2022-10-18 23:08:50 -0700

    Fixed player Script
    Player.gd is being used as the main Player Script.
    
    added code from old player script to Player.gd
    
    player is able to move if W key is pressed , moves in the same directions the player is looking at.

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -5,3 +5,19 @@ func _on_Hurtbox_area_entered(area):
 	if projectile.is_in_group("enemy"):
 		take_damage(projectile.damage) 
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		var movement = event.relative
+		rotation.x += deg2rad(movement.y * .2 )
+		rotation.x = clamp(rotation.x, deg2rad(-90), deg2rad(90))
+		rotation.y += -deg2rad(movement.x * .2)
+		rotation.y = clamp(rotation.y, deg2rad(-180), deg2rad(180))
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("move_forward"):
+			add_force(self.transform.basis.z*100, Vector3.ZERO)
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+func _ready():
+
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)

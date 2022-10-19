d12dc407ff51a1a73286bb16ef57cf41078978da
Kamran Haq <KamranHaq@protonmail.com>
2022-10-18 22:37:47 -0700

    Simple Flee and Chase AI
    Pretty Buggy. Not sure if it needs better tuning or a change in approach to fleeing. Could also make better use of rolling in maneuvers.

--- Assets/Scripts/Enemy.gd
+++ Assets/Scripts/Enemy.gd
@@ -4,12 +4,17 @@ onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var pitch_controller = $PitchController
 onready var yaw_controller = $YawController
 onready var roll_controller = $RollController
-var target_x = 0
-var target_y = 0
-var target_z = 0
+
+var state = IDLE
+
+enum {
+	IDLE,
+	SEEKING,
+	FLEEING
+}
 
 func _ready():
-	set_main_thruster(1)
+	set_main_thruster(0)
 
 func _on_Hurtbox_area_entered(area):
 	var projectile = area.get_parent()
@@ -20,21 +25,48 @@ func _on_Hurtbox_area_entered(area):
 
 	
 func _physics_process(delta):
+	var player_dist = self.translation.distance_to(player.translation)
+	if player_dist < 30:
+		state = FLEEING
+	elif (player_dist < 700 and player_dist > 100 ):
+		state = SEEKING
+	
+	#if self.translation.distance_to(player.translation) >= 1000:
+		#state = IDLE
 
-	var v1 = Vector2(self.translation.x, self.translation.z)
-	var v2 = Vector2(player.translation.x, player.translation.z)
-	var v3 = (v1 - v2).angle()
-	v3 += PI/2
-	
-	add_yaw(yaw_controller.calculate(-v3, rotation.y))
-	
-	v1 = Vector2(self.translation.y, self.translation.z)
-	v2 = Vector2(player.translation.y, player.translation.z)
-	v3 = (v1 - v2).angle()
-	v3 += PI/2
-	
-	add_pitch(pitch_controller.calculate(v3, rotation.x))
-	add_roll(roll_controller.calculate(0, self.rotation.z))
-	
-	if Input.is_action_pressed("ui_down"):
-		add_torque(Vector3.DOWN)
+	match state:
+		IDLE:
+			set_main_thruster(0)
+		SEEKING:
+			set_main_thruster(1)
+			var v1 = Vector2(self.translation.x, self.translation.z)
+			var v2 = Vector2(player.translation.x, player.translation.z)
+			var v3 = (v1 - v2).angle()
+			v3 += PI/2
+			
+			add_yaw(yaw_controller.calculate(-v3, rotation.y))
+			
+			v1 = Vector2(self.translation.y, self.translation.z)
+			v2 = Vector2(player.translation.y, player.translation.z)
+			v3 = (v1 - v2).angle()
+			v3 += PI/2
+			
+			add_pitch(pitch_controller.calculate(v3, rotation.x))
+			add_roll(roll_controller.calculate(0, self.rotation.z))
+		FLEEING:
+			set_main_thruster(1)
+			var v1 = Vector2(self.translation.x, self.translation.z)
+			var v2 = Vector2(player.translation.x, player.translation.z)
+			var v3 = (v1 - v2).angle()
+			v3 += PI/2
+			
+			add_yaw(yaw_controller.calculate(v3, rotation.y))
+			
+			v1 = Vector2(self.translation.y, self.translation.z)
+			v2 = Vector2(player.translation.y, player.translation.z)
+			v3 = (v1 - v2).angle()
+			v3 += PI/2
+			
+			add_pitch(pitch_controller.calculate(-v3, rotation.x))
+			add_roll(roll_controller.calculate(0, self.rotation.z))
+

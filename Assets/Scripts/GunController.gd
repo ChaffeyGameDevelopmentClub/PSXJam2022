ad9e7a4da0ec53caeca05577d97c87836c75e67d
Bdog12 <brandonjo1034@gmail.com>
2022-10-23 21:46:46 -0700

    Added reload to gun
    after shooting 30 shots the gun will go on a 3 sec cooldown.

--- Assets/Scripts/GunController.gd
+++ Assets/Scripts/GunController.gd
@@ -3,13 +3,19 @@ extends Node
 
 
 onready var RateOfFire_timer = $Timer
+onready var Reload_Timer = $Timer2
 var can_shoot = true
+var Is_Reloading = false
+var Bullets_shot = 0
+var Max_Bullet = 30
 
 export(PackedScene) var Bullet
 export var muzzle_speed = 20
-export var Seconds_Between_Shoots = .1
+export var Seconds_Between_Shoots = .14
+export var Reload_Time = 3
 func _ready():
 	RateOfFire_timer.wait_time = Seconds_Between_Shoots /1.0
+	Reload_Timer.wait_time = Reload_Time
 	pass
 
 
@@ -17,18 +23,39 @@ func _process(delta):
 	pass
 	
 func _shoot():
+	if !Is_Reloading:
 		if can_shoot:
 			var New_bullet = Bullet.instance()
 			New_bullet.global_transform =$Spawn_location_Bullet.global_transform
 			#New_bullet.speed = muzzle_speed
 			var scene_root = get_tree().get_root().get_children()[0]
 			scene_root.add_child(New_bullet)
-			can_shoot = false
-			RateOfFire_timer.start()
-		
-		
+			Bullets_shot +=1
+			print(Bullets_shot)
+			if Bullets_shot >= Max_Bullet:
+				can_shoot = false
+				Reloading()
+			elif can_shoot:
+				can_shoot = false
+				RateOfFire_timer.start()
+			
 	
 
 
-func _on_Timer_timeout():
+func _on_Timer_timeout(): #Rate of fire timer
+	can_shoot = true
+	
+func Reloading():
+		Is_Reloading = true
+		if Is_Reloading:
+			can_shoot = false
+			print("Is reloading")
+			Reload_Timer.start()
+		
+
+func _on_Timer2_timeout():	#Reload timer
+	print("Finished Reloading ")
+	Is_Reloading = false
+	Bullets_shot = 0
+	Is_Reloading = false
 	can_shoot = true
